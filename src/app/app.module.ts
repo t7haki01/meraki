0794597aff992138db041bbadadf6e03bbd4d157
t7haki01 <t7haki01@students.oamk.fi>
2019-07-15 13:00:57 +0300

    about text updated, unused dependencies removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GuideComponent } from './guide/guide.component';
@@ -42,6 +43,6 @@ import { NearComponent } from './near/near.component';
     HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
